4a3737d0eccc00c9a46be44a2d4102780b1b910a
lien-testnet002 <testnetver2@protonmail.com>
2020-07-09 17:40:03 +0900

    gh-pages

---
+++ precache-manifest.3a2e9aeebbbd039049d7fa66f75c67ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a423b7b75877dd9edd401775a5187d8",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "dc6fc714b2cfd7426b26",
+    "url": "/test/static/css/main.92f13f41.chunk.css"
+  },
+  {
+    "revision": "665fbc81e74af4a7558b",
+    "url": "/test/static/js/2.56a90354.chunk.js"
+  },
+  {
+    "revision": "bd1a193268b514dec25e55f32db18858",
+    "url": "/test/static/js/2.56a90354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6fc714b2cfd7426b26",
+    "url": "/test/static/js/main.a724dcf2.chunk.js"
+  },
+  {
+    "revision": "486feccb858fb4dca59b",
+    "url": "/test/static/js/runtime-main.cb408556.js"
+  },
+  {
+    "revision": "9cf70d0cf353f5ac9c1e6bdec93f4f0d",
+    "url": "/test/static/media/chainlink_badge.9cf70d0c.svg"
+  },
+  {
+    "revision": "a0103549b5dca7100830a06fc1a09cb9",
+    "url": "/test/static/media/metamask.a0103549.svg"
+  }
+]);
